df44d697a2000d1d860e64811b3e8a052a872ab2
Ruslan <rmatusevich@techvice.org>
2023-03-14 10:40:04 +0100

    fix stucture

--- src/pages/main-page/index.tsx
+++ src/pages/main-page/index.tsx
@@ -1,6 +1,18 @@
+import { ReactNode } from 'react'
+import iconNote from 'assets/note-icon.svg';
+import styles from './styles.module.scss';
+
 const MainPage: React.FC = () => {
+
+  const renderPlaceholder = (): ReactNode => <div className={styles.root}>
+  <div>
+    <img src={iconNote} alt="note" />
+    <h1>Select or create note</h1>
+  </div>
+</div>
+
   return (
-   <div>notes</div>
+   <div>{renderPlaceholder()}</div>
   );
 };
 
--- src/pages/note/index.tsx
+++ src/pages/note/index.tsx
@@ -1,25 +1,14 @@
-import { ReactNode } from 'react'
 import { useSelector } from 'react-redux';
 import { selectSelectedNote } from 'entities/notes/model';
 import NoteEditor from 'features/note-editor';
-import iconNote from 'assets/note-icon.svg';
-import styles from './styles.module.scss';
 
 const Note: React.FC = () => {
   const activeNote = useSelector(selectSelectedNote);
 
-  const renderPlaceholder = (): ReactNode => <div className={styles.root}>
-    <div>
-      <img src={iconNote} alt="note" />
-      <h1>Select or create note</h1>
-    </div>
-  </div>
   
   return (
     <div>
-      {activeNote
-        ? <NoteEditor />
-        : renderPlaceholder()}
+      <NoteEditor />
     </div>
   );
 };
